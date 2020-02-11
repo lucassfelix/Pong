8b81579df5470d3805c091102519c59e7323d3d6
Lucas Salaverry Félix <lsfelix321@gmail.com>
2020-02-11 08:20:59 -0300

    Prototype player movement and ball bounce

---
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+
+    private Rigidbody2D _rb;
+    private float minVelocity = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       _rb = GetComponent<Rigidbody2D>();
+
+        _rb.velocity = Vector2.left * 2f;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var relativeVelocity = other.GetContact(0).relativeVelocity;
+        Debug.Log("Enter Collision Velocity:" + relativeVelocity);
+        _rb.velocity = other.collider.name == "Borders" ? new Vector2(-relativeVelocity.x, relativeVelocity.y): new Vector2(relativeVelocity.x, relativeVelocity.y);
+        Debug.Log("Exit Collision Velocity:" + _rb.velocity);
+    }
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+}
---
+++ Pong/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(1,100)]
+    public float speed = 1f;
+
+    public float maxSpeed = 5f;
+    
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+    
+    void Start()
+    {
+        if (Camera.main==null) {Debug.LogError("Camera.main not found."); return;}
+
+        var cam = Camera.main;
+        
+        _rb = GetComponent<Rigidbody2D>();
+        
+        var width = cam.pixelWidth;
+        var height = cam.pixelHeight;
+        
+        var initialPosition = Camera.main.ScreenToWorldPoint( new Vector3(width / 6f,height/2f,0));
+
+        _rb.position = initialPosition;
+    }
+    
+    void Update()
+    {
+        _movement = new Vector2 (0,Input.GetAxis("Vertical"));
+    }
+    
+    private void FixedUpdate()
+    {
+        Movement(_movement);
+
+        if (_rb.velocity.y > maxSpeed)
+        {
+            _rb.velocity = new Vector2(0,maxSpeed);
+        }
+        
+        if (_rb.velocity.y < -maxSpeed)
+        {
+            _rb.velocity = new Vector2(0,-maxSpeed);
+        }
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        _rb.velocity += speed * direction;
+    }
+}
